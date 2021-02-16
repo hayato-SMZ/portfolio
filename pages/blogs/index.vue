3f096d728121b8d8b7623d7dc22ccd14df954b80
清水隼人 <63823387+hy-shimizu-d1g@users.noreply.github.com>
2021-02-16 22:20:54 +0900

    ブログの機能を追加

---
+++ pages/blogs/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="b in blogs" :key="b.slug">
+      <nuxt-link :to="'/blogs/' + b.slug">{{ b.title }} {{ b.date }}</nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const limit = 10;
+    const query = await $content('blogs' || 'index').limit(limit)
+    const blogs = await query.fetch()
+    console.log(blogs);
+    return { blogs }
+  },
+}
+</script>
